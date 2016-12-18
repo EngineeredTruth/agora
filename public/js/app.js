65f8cc2e9ed75c14ec448c0b47719ec8bdd0c005
Engineered Truth <Tangoforthemango@gmail.com>
2016-12-18 11:35:53 -0800

    resource page almost done

--- public/js/app.js
+++ public/js/app.js
@@ -21,6 +21,10 @@ var app = angular.module('app',['ui.router','ui.bootstrap',"pageslide-directive"
         url:'/paul',
         templateUrl: './views/paul.html'
       })
+      .state('resources', {
+        url:'/resources',
+        templateUrl: './views/resources.html'
+      })
 
       $urlRouterProvider.otherwise('/');
 })
