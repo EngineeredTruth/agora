9cc03438306f2e49359a5b2a263c6ef73678e001
Matt <EnineeredTruth@gmail.com>
2017-01-17 18:49:44 -0800

    changes have been made

--- public/js/MainCtrl.js
+++ public/js/MainCtrl.js
@@ -6,4 +6,27 @@ app.controller('MainCtrl', function($scope, $location){
     $scope.checked = !$scope.checked;
   }
 
+  $scope.location = () => {
+    console.log($location)
+  }
+
+
+
+  $scope.$on('$locationChangeStart', function(event, nextUrl, currentUrl) {
+    console.log('event: ', event);
+    console.log('nextUrl: ', nextUrl);
+    console.log('currentUrl: ', currentUrl);
+
+    var patt = /contact$/
+    if(patt.test(nextUrl)){
+      $scope.toggle_contact = false;
+      console.log($scope.toggle_contact)
+    } else {
+      $scope.toggle_contact = true;
+      console.log($scope.toggle_contact)
+    }
+
+
+  });
+
 });
