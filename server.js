dd86688fe0cb79223186debf49528b5d2a04a743
Matt <EnineeredTruth@gmail.com>
2017-02-07 14:37:05 -0800

    ready to be hosted

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+app.use(require('prerender-node'));
+
 app.use(express.static(__dirname + '/public'));
 
-app.listen(2000, function() {
-    console.log('Hosting port', 2000);
+app.listen(2121, function() {
+    console.log('Hosting port', 2121);
 });
